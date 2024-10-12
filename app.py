1f2f532adc95afe3d70922a236cc4c8b38e4c08f
carrenogf <carrenogf@gmail.com>
2024-10-12 08:57:00 -0300

    añadido ajustes de series

--- app.py
+++ app.py
@@ -5,24 +5,84 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import acf, pacf
+import numpy as np
+
+
 
 st.set_page_config(layout="wide")
+st.title("Series Diarias")
 
 # Load data
 df = pd.read_excel("dataset/series_diarias.xlsx")
+df = df.sort_values("FECHA",ascending=True)
 df.set_index("FECHA", inplace=True)
-series = df.columns
+columnas = list(df.columns)
+series = [serie for serie in columnas if serie not in ["VALOR_UVA", "TC_MINORISTA"]]
+columns0 = st.columns(5)
+
+
+# funciones
+@st.dialog("Guardar columna nueva")
+def guardar_columna(df):
+    new_column = st.text_input("Ingrese el nombre de la columna")
+    if st.button("guardar"):
+        dataframe = pd.read_excel("dataset/series_diarias.xlsx")
+        dataframe = dataframe.sort_values("FECHA")
+        dataframe.set_index("FECHA", inplace=True)
+        if new_column:
+            if new_column not in df.columns:
+                df[new_column] = df[option]
+                df = df[[new_column]]
+                dataframe = pd.merge(dataframe, df, left_index=True, right_index=True)
+                dataframe.to_excel("dataset/series_diarias.xlsx")
+                st.rerun()
+            else:
+                st.write("El nombre de la columna ya existe")
+
+
 
 # filtrar serie y eliminar NAs
-option = st.selectbox(
-    "Elegir Serie",
-    series
-)
+with columns0[0]:
+    option = st.selectbox(
+        "Elegir Serie",
+        series
+    )
+    
 if option:
+    with columns0[1]: # ajuste logaritmico
+        ajuste1 = st.radio("Ajuste 1", ["Ninguno", "Logarítmico"])
+        
+        if ajuste1 == "Logarítmico":
+            df[option] = np.log(df[option])
+        
+    with columns0[2]: # ajuste por infalcion con uva o tipo de cambio
+        ajuste2 = st.radio("Ajuste 2", ["Ninguno", "VALOR_UVA","TC_MINORISTA"])
+        if ajuste2 == "VALOR_UVA":
+            df = df.dropna(subset=["VALOR_UVA"])
+            df["original"] = df[option]
+            df[option] = df[option] / df["VALOR_UVA"] * df["VALOR_UVA"].iloc[-1]
+        elif ajuste2 == "TC_MINORISTA":
+            df = df.dropna(subset=["TC_MINORISTA"])
+            df["original"] = df[option]
+            df[option] = df[option] / df["TC_MINORISTA"] * df["TC_MINORISTA"].iloc[-1]          
+        
+    with columns0[3]:
+        ajuste3 = st.radio("Ajuste 2", ["Ninguno", "boxcox"])
+        if ajuste3 == "boxcox":
+            from scipy.stats import boxcox
+            df = df.dropna(subset=[option])
+            df[option] = boxcox(df[option])[0]
+    with columns0[4]:
+        st.write("Guardar datos ajustados como columna nueva en el dataset")
+        if st.button("Guadar columna"):
+            guardar_columna(df)
+
+
+        
+        
     df = df[option] # filtra la serie desde lo seleccionado por el usuario
     df = df.dropna() # elimina los NAs
 
-    st.title("Series Diarias")
     # gráfico de la serie
     with st.container():
         fig = px.line(df, title=f"Serie Diaria de {option}")
